42656d99411aae22d09d7028d2d1be9c00356a36
rTonmoy <rahman53166@gmail.com>
2024-08-26 23:50:16 -0700

    demo

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -9,49 +9,49 @@ import GalleryImg7 from "../../assets/images/gallery/g7.jpg"
 
 const Gallery = () => {
 
-    var images = [
-        {
-            src: GalleryImg1,
-            desc: "Person wearing shoes",
-            sub: "Gift Habeshaw"
-        },
-        {
-            src: GalleryImg3,
-            desc: "Blonde woman wearing sunglasses smiling at the camera ",
-            sub: "Dmitriy Frantsev"
-        },
-        {
-            src: GalleryImg6,
-            sub: "Harry Cunningham"
-        },
-        {
-            src: GalleryImg4,
-            desc: "Jaipur , Rajasthan India",
-            sub: "Liam Baldock"
-        },
-        {
-            src: GalleryImg7,
-            sub: "Verne Ho"
-        },
-        {
-            src: GalleryImg6,
-            desc: "Rann of kutch , India",
-            sub: "Hari Nandakumar"
-        },
-    ];
+    // var images = [
+    //     {
+    //         src: GalleryImg1,
+    //         desc: "Person wearing shoes",
+    //         sub: "Gift Habeshaw"
+    //     },
+    //     {
+    //         src: GalleryImg3,
+    //         desc: "Blonde woman wearing sunglasses smiling at the camera ",
+    //         sub: "Dmitriy Frantsev"
+    //     },
+    //     {
+    //         src: GalleryImg6,
+    //         sub: "Harry Cunningham"
+    //     },
+    //     {
+    //         src: GalleryImg4,
+    //         desc: "Jaipur , Rajasthan India",
+    //         sub: "Liam Baldock"
+    //     },
+    //     {
+    //         src: GalleryImg7,
+    //         sub: "Verne Ho"
+    //     },
+    //     {
+    //         src: GalleryImg6,
+    //         desc: "Rann of kutch , India",
+    //         sub: "Hari Nandakumar"
+    //     },
+    // ];
 
-    var settings = {
-        columnCount: {
-            default: 3,
-            mobile: 2,
-            tab: 3
-        },
-        mode: "dark",
-        enableZoom:false,
-    };
+    // var settings = {
+    //     columnCount: {
+    //         default: 3,
+    //         mobile: 2,
+    //         tab: 3
+    //     },
+    //     mode: "dark",
+    //     enableZoom:false,
+    // };
     return (
         // <Lightroom images={images} settings={settings} />
-        <h1>hello</h1>
+        <h1><img src={GalleryImg1} alt="" /></h1>
    );
 }
 
