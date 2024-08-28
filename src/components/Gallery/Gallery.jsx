360d7c5459a1f7d977c6ff45a8b85911aa3d1201
rTonmoy <rahman53166@gmail.com>
2024-08-27 22:44:23 -0700

    demo

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,58 +1,41 @@
-import React from 'react'
-// import Lightroom from 'react-lightbox-gallery'
-import GalleryImg1 from "../../assets/images/gallery/g1.jpg"
-import GalleryImg3 from "../../assets/images/gallery/g3.jpg"
-import GalleryImg4 from "../../assets/images/gallery/g4.jpg"
-import GalleryImg6 from "../../assets/images/gallery/g6.jpg"
-import GalleryImg7 from "../../assets/images/gallery/g7.jpg"
-
+import React from 'react';
+import GalleryImg1 from "../../assets/images/gallery/g1.jpg";
+import GalleryImg2 from "../../assets/images/gallery/g2.jpg";
+import GalleryImg3 from "../../assets/images/gallery/g3.jpg";
+import GalleryImg4 from "../../assets/images/gallery/g4.jpg";
+import GalleryImg6 from "../../assets/images/gallery/g6.jpg";
+import GalleryImg7 from "../../assets/images/gallery/g7.jpg";
+import "../Gallery/gallery.css";
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Gallery = () => {
 
-    // var images = [
-    //     {
-    //         src: GalleryImg1,
-    //         desc: "Person wearing shoes",
-    //         sub: "Gift Habeshaw"
-    //     },
-    //     {
-    //         src: GalleryImg3,
-    //         desc: "Blonde woman wearing sunglasses smiling at the camera ",
-    //         sub: "Dmitriy Frantsev"
-    //     },
-    //     {
-    //         src: GalleryImg6,
-    //         sub: "Harry Cunningham"
-    //     },
-    //     {
-    //         src: GalleryImg4,
-    //         desc: "Jaipur , Rajasthan India",
-    //         sub: "Liam Baldock"
-    //     },
-    //     {
-    //         src: GalleryImg7,
-    //         sub: "Verne Ho"
-    //     },
-    //     {
-    //         src: GalleryImg6,
-    //         desc: "Rann of kutch , India",
-    //         sub: "Hari Nandakumar"
-    //     },
-    // ];
-
-    // var settings = {
-    //     columnCount: {
-    //         default: 3,
-    //         mobile: 2,
-    //         tab: 3
-    //     },
-    //     mode: "dark",
-    //     enableZoom:false,
-    // };
     return (
-        // <Lightroom images={images} settings={settings} />
-        <h1><img src={GalleryImg1} alt="" /></h1>
-   );
+        <Container>
+            <Row className='gy-4'>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg1} alt="" />
+                </Col>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg2} alt="" />
+                </Col>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg3} alt="" />
+                </Col>
+            </Row>
+            <Row className='gy-4 tmi'>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg4} alt="" />
+                </Col>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg6} alt="" />
+                </Col>
+                <Col xs="12" sm="6" md="4">
+                    <img className='img-fluid ami' src={GalleryImg7} alt="" />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-export default Gallery
+export default Gallery;
